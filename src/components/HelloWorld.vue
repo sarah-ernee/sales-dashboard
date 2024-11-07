f91a090094a1e16fe5a52907c0a3f887009e0d77
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-11-07 19:06:15 +0800

    initialize project scaffold + base component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,166 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="sales-dashboard">
+    <h2>Sales Records</h2>
+
+    <v-btn class="sales-dashboard__filter" variant="flat"> Filter Popup </v-btn>
   </div>
+
+  <v-data-table
+    :headers="headers"
+    :items="orders"
+    :items-per-page="10"
+    color="sales-records"
+  >
+    <template v-slot:bottom></template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  setup() {
+    const headers = [
+      {
+        title: "Order No",
+        key: "orderNo",
+        align: "start",
+        sortable: true,
+      },
+      {
+        title: "Customer Name",
+        key: "customerName",
+        align: "start",
+        sortable: true,
+      },
+      {
+        title: "Status",
+        key: "status",
+        align: "start",
+        sortable: true,
+      },
+      {
+        title: "Category",
+        key: "category",
+        align: "start",
+        sortable: true,
+      },
+      {
+        title: "Country",
+        key: "country",
+        align: "start",
+        sortable: true,
+      },
+      {
+        title: "Created Date",
+        key: "createdDate",
+        align: "start",
+        sortable: true,
+      },
+    ];
+
+    const orders = [
+      {
+        orderNo: 1,
+        customerName: "Kivell",
+        status: "Accepted",
+        category: "Electronics",
+        country: "United Kingdom",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 2,
+        customerName: "Jardine",
+        status: "Processing",
+        category: "Furniture",
+        country: "Russia",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 3,
+        customerName: "Gill",
+        status: "Rejected",
+        category: "Stationery",
+        country: "German",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 4,
+        customerName: "Sor’vino",
+        status: "Open",
+        category: "Furniture",
+        country: "Singapore",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 5,
+        customerName: "Jones",
+        status: "Rejected",
+        category: "Sports",
+        country: "German",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 6,
+        customerName: "Andrews",
+        status: "Processing",
+        category: "Electronics",
+        country: "Malaysia",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 7,
+        customerName: "Jardine",
+        status: "Processing",
+        category: "Sports",
+        country: "German",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 8,
+        customerName: "Thompson",
+        status: "Accepted",
+        category: "Hardware",
+        country: "Malaysia",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 9,
+        customerName: "Jones",
+        status: "Open",
+        category: "Furniture",
+        country: "Taiwan",
+        createdDate: "1/23/2019",
+      },
+      {
+        orderNo: 10,
+        customerName: "Morgan",
+        status: "Processing",
+        category: "Sports",
+        country: "China",
+        createdDate: "1/23/2019",
+      },
+    ];
+
+    return {
+      headers,
+      orders,
+    };
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+.sales-dashboard {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 0 1.5rem 0;
+
+  &__filter {
+    text-transform: capitalize;
+    background-color: #8bbce3;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.sales-records {
+  margin: 1rem 0 0 0;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/pages/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  // Add additional routes here
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+export default router;
